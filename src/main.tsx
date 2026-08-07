f7dd62f1d226dfcdaf1f754ad81f2fdd9e2ac6f5
soundbier <soundbier@gmail.com>
2026-08-07 16:03:07 +0200

    main.tsx aktualisieren

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './global.css';
+import { App } from './App';
+import './global.css'; // <-- Ohne das hier gibt es keinen Darkmode und kein Fullscreen!
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
